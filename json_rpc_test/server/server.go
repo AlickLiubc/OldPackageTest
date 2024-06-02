3db238157f595400d0ab6a4901796dc6837abd24
AlickLiubc <nrlbc@sina.com>
2024-06-03 00:25:03 +0800

    补充代码

---
+++ json_rpc_test/server/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+)
+
+type HelloService struct{}
+
+func (s *HelloService) Hello(request string, reply *string) error {
+	// 返回值是通过修改reply的值
+	*reply = "hello, " + request
+
+	return nil
+}
+
+func main() {
+	// 1. 实例化一个Server
+	listener, _ := net.Listen("tcp", ":1234")
+	// 2. 注册处理handler
+	_ = rpc.RegisterName("HelloService", &HelloService{})
+	// 3. 启动服务
+	// 当一个新的连接进来的时候
+	for {
+		conn, _ := listener.Accept()
+		go rpc.ServeCodec(jsonrpc.NewServerCodec(conn))
+	}
+
+}
